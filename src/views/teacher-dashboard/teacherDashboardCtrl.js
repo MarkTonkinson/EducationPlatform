49f1e471d9d012b0189f4caef8598bc023406f0f
Mark Tonkinson <DHTMaster@gmail.com>
2015-03-12 17:36:55 -0600

    Made some changes to the physical appearance fo the dashboard.  Learned that angular automatically caches directive templates and won't update unless you disable cache in the dev tools

--- src/views/teacher-dashboard/teacherDashboardCtrl.js
+++ src/views/teacher-dashboard/teacherDashboardCtrl.js
@@ -16,7 +16,9 @@ angular.module('EducationPlatform')
 	//TODO: teacher-links has to be reworked because it is an attribute and it needs to pass with links
 	//profile is similar in that it needs to have details passed
 	$scope.directives = {
-		list: ['quick-quiz', 'quick-notes', 'student-checklist', 'teacher-links'],
+		//when adding to this list, the name needs to correlate to the actual directive name- we can rename the directive name 
+		//if needed
+		list: ['Quick Quiz', 'Quick Notes', 'Student Checklist', 'Teacher Links'],
 		changeWidth: function(dirName, newWidth){
 
 		},
@@ -35,19 +37,19 @@ angular.module('EducationPlatform')
 	}
 
 	$scope.createDirective = function(directive){
-		
+		directive = directive.toLowerCase().replace(' ','-')
 		 //this was the important key to solving the problem of passing an object as a string!! But would it keep reference if the name changed?
         var obj = $scope.user
         obj = JSON.stringify(obj)
         //var directive = LazyDirectiveLoader.loadDirective('edu-profile', 'userprofile=' + obj);
         var directive = LazyDirectiveLoader.loadDirective(directive)
-        console.log('directive ', directive)
+        //console.log('directive ', directive)
 
         var newDirective = angular.element(directive);
 
 		var divs = angular.element(document).find('section');
 
-		console.log(divs)
+		//console.log(divs)
 
 		divs.append(newDirective)
 
